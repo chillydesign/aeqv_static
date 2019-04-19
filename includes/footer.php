dd23618d416baf3efa59b4b949f25990bb007445
Mélissa Rissel <rissel.melissa@gmail.com>
2019-04-19 10:33:01 +0200

    version numbers

--- includes/footer.php
+++ includes/footer.php
@@ -63,6 +63,6 @@
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/slick.min.js"></script>
     <script type="text/javascript" src="js/lightbox.min.js"></script>
-    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript" src="js/scripts.js?v=2"></script>
 </body>
 </html>
